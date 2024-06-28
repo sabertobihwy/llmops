c0744d8ab986e066c294a14904db26467df099f2
wenyan <hewenyan2023@gmail.com>
2024-06-28 12:41:05 -0400

    Update .gitignore and stop tracking test folder

--- llmops-api/test.py
+++ llmops-api/test.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print("hello world")
+from injector import Injector,inject
+
+
+class A:
+    def print(self):
+        print("aaaaa")
+
+@inject
+class B:
+    def __init__(self,a:A):
+        self.a = a
+
+injector = Injector()
+b_instance = injector.get(B)
+b_instance.a.print()
